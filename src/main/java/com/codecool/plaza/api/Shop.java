7d0907088ea8ae5c931c2b37b88eee6126f11736
Bernáth Máté <bernath.mate.new@gmail.com>
2018-02-13 16:24:46 +0100

    Shop interface is done.

---
+++ src/main/java/com/codecool/plaza/api/Shop.java
@@ -0,0 +1,16 @@
+package com.codecool.plaza.api;
+
+public interface Shop {
+    public String getName();
+    public String getOwner();
+    public boolean isOpen();
+    public void open();
+    public void close();
+    public Product findByName(String name) throws ShopIsClosedException;
+    public boolean hasProduct(long barcode) throws ShopIsClosedException;
+    public void addNewProduct(Product product, int quantity, float price) throws ProductAlreadyExistsException, ShopIsClosedException;
+    public void addProduct(long barcode, int quantity) throws NoSuchProductException, ShopIsClosedException;
+    public Product buyProduct(long barcode) throws NoSuchProductException, OutOfStockException, ShopIsClosedException;
+    public List<Product> buyProducts(long barcode, int quantity) throws NoSuchProductException, OutOfStockException, ShopIsClosedException;
+    public String toString();
+}
