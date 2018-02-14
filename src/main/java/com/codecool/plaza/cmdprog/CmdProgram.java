a429b046e286f906f84a927f5dadf03969977363
Bernáth Máté <bernath.mate.new@gmail.com>
2018-02-14 11:19:40 +0100

    CmdProgram class is created, and the class's skeleton is done.

---
+++ src/main/java/com/codecool/plaza/cmdprog/CmdProgram.java
@@ -0,0 +1,19 @@
+package com.codecool.plaza.cmdprog;
+
+import com.codecool.plaza.api.Product;
+
+import java.util.List;
+
+public class CmdProgram {
+
+    private List<Product> cart;
+    private String[] args;
+
+    public CmdProgram(String[] args) {
+        this.args = args;
+    }
+
+    public void run() {
+
+    }
+}
